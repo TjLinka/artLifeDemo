313c0fc9f0f53dbc83d0742acf375d14132f7796
TjLinka <38112505+TjLinka@users.noreply.github.com>
2020-10-13 12:36:53 +0300

    Auth__page__progress

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,69 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container-fluid p-0">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#"><h3>Artlife</h3></b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <div v-if="isAuth" class="d-lg-flex">
+              <b-nav-item-dropdown text="Профиль" right>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+              </b-nav-item-dropdown>
+              <b-nav-item-dropdown text="Структура" right>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+              </b-nav-item-dropdown>
+              <b-nav-item-dropdown text="Маркетинг" right>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+              </b-nav-item-dropdown>
+              <b-nav-item-dropdown text="Лицевой счёт" right>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+              </b-nav-item-dropdown>
+              <b-nav-item-dropdown text="История покупок" right>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </div>
+            <b-nav-item-dropdown right>
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isAuth: this.$store.state.auth.is_authorized,
+    };
+  },
+  created: () => {},
+};
+</script>
+
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.navbar {
+  background-color: darken($color: cyan, $amount: 15%);
+  // height: 70px;
+}
 </style>
---
+++ src/store/auth.js
@@ -0,0 +1,35 @@
+import backApi from '../assets/backApi';
+
+export default {
+  namespaced: true,
+  state: {
+    auth_status: '',
+    is_authorized: localStorage.getItem('access_token'),
+    acces_token: localStorage.getItem('access_token') || '',
+    role: undefined,
+    name: undefined,
+  },
+  mutations: {},
+  actions: {
+    logging(state, user) {
+      console.log('hello', user);
+      return new Promise((resolve, reject) => {
+        backApi
+          .post('/login', JSON.stringify(user))
+          .then((repsonse) => {
+            const accessToken = repsonse.data.access_token;
+            localStorage.setItem('access_token', accessToken);
+            backApi.defaults.headers.post.access_token = accessToken;
+            resolve(repsonse);
+          })
+          .catch((err) => {
+            state.auth_status = 'error';
+            localStorage.removeItem('access_token');
+            delete backApi.defaults.headers.post.access_token;
+            reject(err);
+          });
+      });
+    },
+  },
+  getters: {},
+};
