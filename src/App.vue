fd55a7961f050bbba1c9f73668e2b837443cb515
es <sergbuks2@gmail.com>
2020-10-19 08:25:36 +0300

    moved the navbar into a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,6 @@
 <template>
   <div id="app">
-    <div class="container-fluid p-0">
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#"><h3>Artlife</h3></b-navbar-brand>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse v-if="is_authorized" id="nav-collapse" is-nav>
-          <b-navbar-nav class="ml-auto">
-            <div class="d-lg-flex">
-              <b-nav-item-dropdown text="Профиль" right>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-              </b-nav-item-dropdown>
-              <b-nav-item-dropdown text="Структура" right>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-              </b-nav-item-dropdown>
-              <b-nav-item-dropdown text="Маркетинг" right>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-              </b-nav-item-dropdown>
-              <b-nav-item-dropdown text="Лицевой счёт" right>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-              </b-nav-item-dropdown>
-              <b-nav-item-dropdown text="История покупок" right>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-                <b-dropdown-item href="#">Lorem, ipsum.</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </div>
-            <b-nav-item-dropdown right>
-              <template v-slot:button-content>
-                <em>{{ agentname }}</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#" v-on:click="out">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+    <TheNavBar></TheNavBar>
     <main>
       <transition name="fade" mode="out-in">
         <router-view />
@@ -50,23 +10,12 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
+import TheNavBar from '@/components/TheNavBar.vue';
 
 export default {
+  components: { TheNavBar },
   data() {
     return {};
-  },
-  created: () => {},
-  computed: {
-    ...mapState('auth', ['is_authorized', 'agentname']),
-  },
-  methods: {
-    ...mapActions('auth', ['logout']),
-    out() {
-      this.logout().then(() => {
-        this.$router.push('/login');
-      });
-    },
   },
 };
 </script>
