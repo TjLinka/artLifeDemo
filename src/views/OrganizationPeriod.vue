60532bfec8587028bd4acb161df7896bdd7b4632
TjLink <barawka087@gmail.com>
2020-10-20 17:31:44 +0300

    BonusHistoty and OrgPeriod

--- src/views/OrganizationPeriod.vue
+++ src/views/OrganizationPeriod.vue
@@ -2,18 +2,47 @@
   <div class="licevoischet__page">
     <div class="container">
       <h2 class="page__title">Организация текущего периода</h2>
-      <div>
-          <span class="mr-3" v-if="selectedFilters.tree_type">{{selectedFilters.tree_type}}</span>
-          <span class="mr-3" v-if="selectedFilters.active_type">
-              {{selectedFilters.active_type}}</span>
-          <span class="mr-3" v-if="selectedFilters.agent_id">{{selectedFilters.agent_id}}</span>
-          </div>
+      <!-- <div>
+        <span class="mr-3" v-if="selectedFilters.tree_type">
+          {{ selectedFilters.tree_type }}
+        </span>
+        <span class="mr-3" v-if="selectedFilters.active_type">
+          {{ selectedFilters.active_type }}
+        </span>
+        <span class="mr-3" v-if="selectedFilters.agent_id">
+          {{ selectedFilters.agent_id }}
+        </span>
+      </div> -->
       <p class="exp_print">
         <span class="mr-3">Печать</span>
         <span class="mr-3">Экспорт в xls</span>
         <span class="mr-3">Экспорт в pdf</span>
       </p>
-      <b-table :fields="fields" :items="entries" head-variant="light"> </b-table>
+      <!-- <vue-ads-table
+        :columns="columns"
+        :rows="rows"
+        :call-children="callChildren"
+      >
+        <template slot="stock_acc_sum_clickable" slot-scope="props">
+          {{ props.row[props.column.property] }}
+        </template>
+      </vue-ads-table> -->
+      <el-table
+        :data="rows"
+        style="width: 100%"
+        row-key="id"
+        border
+        lazy
+        :load="load"
+        :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+      >
+        <el-table-column
+          v-for="(column, index) in columns"
+          :prop="column.property"
+          :key="index"
+          :label="column.title"
+        ></el-table-column>
+      </el-table>
       <div class="row">
         <div class="col text-center search__btn" @click="toggleSearch" v-if="!searchActive">
           Поиск партнера
@@ -27,14 +56,14 @@
               <b-form-radio
                 v-model="selectedFilters.tree_type"
                 name="some-radios-1"
-                value="Полное дерево"
+                value="false"
                 class="d-inline mr-3"
                 >Полное дерево</b-form-radio
               >
               <b-form-radio
                 v-model="selectedFilters.tree_type"
                 name="some-radios-1"
-                value="Директорское"
+                value="true"
                 class="d-inline"
                 >Директорское</b-form-radio
               >
@@ -45,14 +74,14 @@
               <b-form-radio
                 v-model="selectedFilters.active_type"
                 name="some-radios-2"
-                value="Активные"
+                value="true"
                 class="d-inline mr-3"
                 >Активные</b-form-radio
               >
               <b-form-radio
                 v-model="selectedFilters.active_type"
                 name="some-radios-2"
-                value="Все"
+                value="false"
                 class="d-inline"
                 >Все</b-form-radio
               >
@@ -70,7 +99,7 @@
             />
           </div>
           <div class="col-sm-6">
-            <button class="mr-2">Показать</button
+            <button class="mr-2" @click="updateData">Показать</button
             ><button @click="clearSelectedFilters">Сбросить</button>
           </div>
         </div>
@@ -80,57 +109,126 @@
 </template>
 
 <script>
-import 'vue2-datepicker/index.css';
-import 'vue2-datepicker/locale/en';
+// import { VueAdsTable } from 'vue-ads-table-tree';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'vue-ads-table-tree/dist/vue-ads-table-tree.css';
 import backApi from '../assets/backApi';
 
 export default {
   name: 'PointsHistory',
+  components: {
+    // VueAdsTable,
+  },
   data() {
+    const rows = [];
+    const columns = [
+      {
+        property: 'has_children',
+        title: 'ID склада',
+      },
+      {
+        property: 'id',
+        title: 'Р/Номер',
+      },
+      {
+        property: 'name',
+        title: 'ФИО',
+      },
+      {
+        property: 'rank_end',
+        title: 'Ранг',
+      },
+    ];
     return {
       searchActive: false,
-      selectedFilters: {},
-      entries: [],
-      fields: [
-        {
-          key: 'dte',
-          label: 'P/номер / Ранг / ФИО',
-          sortable: true,
-        },
-      ],
+      selectedFilters: {
+        agent_id: null,
+        director_tree: false,
+        active_only: false,
+        get_root: true,
+      },
+      rows,
+      columns,
     };
   },
   mounted() {
-    // backApi.get('agent/points-detail').then((Response) => {
-    //   this.entries = Response.data.entries;
-    // });
+    const data = { params: this.selectedFilters };
+    backApi.get('/agents-tree-hist', data).then((response) => {
+      this.rows = response.data.entries;
+      this.rows.forEach((e) => {
+        // eslint-disable-next-line no-underscore-dangle
+        e.hasChildren = e.has_children;
+        // eslint-disable-next-line no-underscore-dangle
+      });
+    });
   },
   computed: {},
   methods: {
-    getSelectedDataRange() {
-      // eslint-disable-next-line max-len
-      if (this.rangeDate[0] != null && this.rangeDate[1] != null) {
-        backApi
-          .get('agent/points-detail', {
-            params: {
-              beg_dte: String(this.rangeDate[0]),
-              end_dte: String(this.rangeDate[1]),
-            },
-          })
-          .then((Response) => {
-            this.entries = Response.data.entries;
-          });
-      } else {
-        backApi.get('agent/points-detail').then((Response) => {
-          this.entries = Response.data.entries;
+    load(tree, treeNode, resolve) {
+      const data = {
+        params: {
+          agent_id: null,
+          director_tree: false,
+          active_only: false,
+          get_root: false,
+        },
+      };
+      backApi.get('/agents-tree-hist', data).then((Response) => {
+        const result = Response.data.entries.slice(1);
+        result.forEach((e) => {
+        // eslint-disable-next-line no-underscore-dangle
+          e.hasChildren = e.has_children;
+        // eslint-disable-next-line no-underscore-dangle
         });
-      }
+        resolve(result);
+      });
     },
     toggleSearch() {
       this.searchActive = !this.searchActive;
     },
     clearSelectedFilters() {
-      this.selectedFilters = {};
+      const data = {
+        params: {
+          agent_id: null,
+          director_tree: false,
+          active_only: false,
+          get_root: true,
+        },
+      };
+      this.selectedFilters = {
+        agent_id: null,
+        director_tree: false,
+        active_only: false,
+        get_root: true,
+      };
+      backApi.get('/agents-tree-hist', data).then((Response) => {
+        this.rows = Response.data.entries;
+        this.rows.forEach((e) => {
+          // eslint-disable-next-line no-underscore-dangle
+          e.hasChildren = e.has_children;
+          // eslint-disable-next-line no-underscore-dangle
+        });
+      });
+      this.searchActive = !this.searchActive;
+    },
+    updateData() {
+      const data = {
+        params: {
+          agent_id: this.selectedFilters.agent_id,
+          director_tree: this.selectedFilters.tree_type,
+          active_only: this.selectedFilters.active_type,
+          get_root: false,
+        },
+      };
+      backApi.get('/agents-tree-hist', data).then((Response) => {
+        this.rows = Response.data.entries;
+        this.rows.forEach((e) => {
+          // eslint-disable-next-line no-underscore-dangle
+          e.hasChildren = e.has_children === 1;
+          // eslint-disable-next-line no-underscore-dangle
+          e._id = 'clickable';
+        });
+      });
       this.searchActive = !this.searchActive;
     },
   },
@@ -154,6 +252,9 @@ export default {
   }
   & .search__btn {
     cursor: pointer;
+    margin-bottom: 30px;
+    text-transform: uppercase;
+    font-weight: bold;
   }
   & .organization__modal {
     //   position: absolute;
@@ -185,5 +286,10 @@ export default {
       }
     }
   }
+}
+</style>
+<style>
+.el-table__indent{
+  padding-left: 0 !important;
 }
 </style>
