0727fc055e8e8d8833cd33fa0d56e7b87da89ead
TjLink <barawka087@gmail.com>
2020-12-29 19:28:26 +0300

    РедакИтирование

--- src/views/MyInfoEdit.vue
+++ src/views/MyInfoEdit.vue
@@ -14,7 +14,7 @@
             <path d="M18 5H3.83L7.41 1.41L6 0L0 6L6 12L7.41 10.59L3.83 7H18V5Z" fill="#32AAA7" />
           </svg>
         </p>
-        Редакитирование личных данных
+        Редактирование личных данных
       </h2>
       <div class="myfoto">
         <img src="../assets/imgs/unnamed 1.png" alt="" />
