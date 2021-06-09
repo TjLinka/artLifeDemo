1b9fe75b3d12342be638ea10a358c6fc2af66698
TjLink <barawka087@gmail.com>
2021-06-09 11:57:48 +0300

    111

--- src/views/AuthIntegration.vue
+++ src/views/AuthIntegration.vue
@@ -8,13 +8,13 @@ import { mapActions } from 'vuex';
 export default {
   name: 'AuthIntegration',
   mounted() {
-    console.log(this.$route.query);
+    this.logout();
     this.loginIntegration(this.$route.query).then(() => {
       this.$router.push(this.$route.query.redirect_url);
     });
   },
   methods: {
-    ...mapActions('auth', ['loginIntegration']),
+    ...mapActions('auth', ['loginIntegration', 'logout']),
   },
 };
 </script>
