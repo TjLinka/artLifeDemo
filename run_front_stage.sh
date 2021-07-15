70474f27996561fee0b1ac56927c7c223d41a347
TjLink <barawka087@gmail.com>
2021-07-15 16:48:03 +0300

    111

--- run_front_stage.sh
+++ run_front_stage.sh
@@ -1,3 +1,3 @@
 docker rm --force artlife_distr_front_s
 docker build . -t artlife_distr_front_s
-docker run -d -v /etc/localtime:/etc/localtime:ro -p 9057:80 --restart=always --name artlife_distr_front_s artlife_distr_front_s
+docker run -d -v /etc/localtime:/etc/localtime:ro -p 9056:80 --restart=always --name artlife_distr_front_s artlife_distr_front_s
