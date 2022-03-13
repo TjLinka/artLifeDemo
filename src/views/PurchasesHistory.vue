c428d47654f629f499062d32b56fae12225105f5
TjLink <barawka087@gmail.com>
2022-03-13 18:51:42 +0300

    KAN-1117

--- src/views/PurchasesHistory.vue
+++ src/views/PurchasesHistory.vue
@@ -34,7 +34,7 @@
               >
               </date-picker>
           </div>
-          <div class="col" v-if="self_agreementsystem && role !== 'Клиент'">
+          <div class="col" v-if="self_agent_id === 900614973 || (self_agreementsystem && role !== 'Клиент')">
             <router-link to="/new-purchase">
                 <button class="btn_type_1 w50 fr">
                   Создать покупку
@@ -418,7 +418,7 @@ export default {
     });
   },
   computed: {
-    ...mapState('auth', ['self_agreementsystem', 'role']),
+    ...mapState('auth', ['self_agreementsystem', 'role', 'self_agent_id']),
     currentPeriod() {
       try {
         return this.periods[this.periodIndex].comdte;
